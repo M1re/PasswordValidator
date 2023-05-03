d291f0418a0968b64f444335d6361aa1ebc8692f
Konokh Vladyslav <actuallydisrespectful@gmail.com>
2023-05-03 14:22:12 +0300

    Add project files.

---
+++ PasswordValidator.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordValidation
+{
+    public class PasswordValidator
+    {
+        public int PasswordValidation(string filePath)
+        {
+            int validPasswordsCount = 0;
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    Regex pattern = new Regex(@"(\w) (\d+)-(\d+): (\w+)");
+
+                    while (!reader.EndOfStream)
+                    {
+                        Match match = pattern.Match(reader.ReadLine());
+
+                        char symbol = match.Groups[1].Value[0];
+                        int minCount = int.Parse(match.Groups[2].Value);
+                        int maxCount = int.Parse(match.Groups[3].Value);
+                        string password = match.Groups[4].Value;
+
+                        int count = Regex.Matches(password, symbol.ToString()).Count;
+
+                        if (count >= minCount && count <= maxCount)
+                        {
+                            validPasswordsCount++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return validPasswordsCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using PasswordValidation;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    static void Main()
+    {
+        var validator = new PasswordValidator();
+        Console.WriteLine("Number of valid passwords in file: " + validator.PasswordValidation("Password.txt"));
+    }
+}
